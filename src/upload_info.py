ace65737e27e95c5ee7aba3fd4a0436144ed6efa
david138 <lukedavidson06@gmail.com>
2017-08-22 23:05:23 -0400

    Luke adds link and copy button

--- src/upload_info.py
+++ src/upload_info.py
@@ -1,15 +1,16 @@
 import sys
 import tkinter as tk
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
-from PyQt5.QtGui import QFont, QPainter, QColor, QBrush
-from PyQt5.QtCore import Qt, QRect, QPoint
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel
+from PyQt5.QtGui import QFont, QPainter, QColor, QBrush, QIcon, QPixmap
+from PyQt5.QtCore import Qt, QRect, QPoint, QSize
 
 class UploadInfo(QWidget):
     def __init__(self, link):
         super().__init__()
-        self.initUI(link)     
+        self.link = link
+        self.initUI()     
         
-    def initUI(self, link):
+    def initUI(self):
         root = tk.Tk()
         self.screen_w = root.winfo_screenwidth()
         self.screen_h = root.winfo_screenheight()
@@ -21,7 +22,25 @@ class UploadInfo(QWidget):
         p.setColor(self.backgroundRole(), Qt.white)
         self.setPalette(p)
 
-        link_button = QPushButton(link, self)
-        link_button.resize(200, 40)
-        link_button.move(100,80)
+        self.create_link()
+        self.create_copy()
 
+    def create_link(self):
+        self.setFont(QFont('SansSerif', 14))
+        link_label = QLabel(self)
+        link_label.setOpenExternalLinks(True)
+        link_label.setText('<a href="' + self.link + '">Snip Uploaded</a>')
+        link_label.resize(200, 40)
+        link_label.move(100,80)
+
+    def create_copy(self):
+        copy_button = QPushButton('', self)
+        copy_button.resize(50, 50)
+        copy_button.move(235, 75)
+        copy_button.clicked.connect(self.on_click)
+
+    def on_click(self, link):
+        cb = tk.Tk()
+        cb.withdraw()
+        cb.clipboard_clear()
+        cb.clipboard_append(self.link)
